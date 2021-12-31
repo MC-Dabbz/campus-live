d0596667b00ed36322d4491c49388e212fd67d94
Mwiza Chiwale <mwizachombachiwale@gmail.com>
2021-12-31 23:44:07 +0200

    added uploading student_id and passport_size_photo

--- public/src/index.js
+++ public/src/index.js
@@ -57,23 +57,25 @@ function getForm(link){
             thing.classList.add("hidden");
             loading(true);
             event.preventDefault();
-            let value = document.getElementById(input_target).value;
-            saveData(input_target, value);
+            
+            if(next_page !== "done-3"){
+                let value = document.getElementById(input_target).value;
+                saveData(input_target, value);
+            }
+            
             if(next_page === "registration/form3.html"){
                 firebase.auth().createUserWithEmailAndPassword(localStorage.getItem('email'), localStorage.getItem('password'))
                         .then((userCredential) => {
                             // Signed in 
                             var user = userCredential.user;
-                            console.log('signed in');
+                            console.log('signed in'+user);
                            
                 }).catch((error) => {
                     loading(false);
                     var errorCode = error.code;
                     var errorMessage = error.message;
                     console.log(errorMessage);
-                    if(errorCode === "auth/email-already-in-use"){
                         getForm("registration/form1.html#error");
-                    }
 
                     // ..
                 });
@@ -108,39 +110,27 @@ function getForm(link){
                     getForm("registration/form7.html");
                 }
                 return false;
-        } else if(input_target == "password"){
-            let password_help = document.getElementById("password-help");
-            // Validate length
-            if(value.length >= 8) {
-                password_help.classList.remove("hidden");
-            } else {
-                password_help.classList.add("hidden");
+        }else if(next_page == "done-3"){
+            let value = document.getElementById("check-box-2").value;
+            if(value === "checked"){
+                submitImages().then((data) =>{
+                    document.getElementById("cover").classList.remove("hidden");
+                    getPage("registration/success.html").then((data)=>{
+                        displayPage(data, page);
+                        loading(false);
+                    // ...
+                }).catch((error)=>{
+                    console.log(error);
+                    loading(false);
+                });
+                    
+                }).catch((error)=>{
+                    console.log(error);
+                });
+            } else{
                 return false;
             }
-            let lowerCaseLetters = /[a-z]/g;
-            if(value.match(lowerCaseLetters)){
-                password_help.classList.remove("hidden");
-            } else {
-                password_help.classList.add("hidden");
-                return false;
-            }
-             // Validate capital letters
-        var upperCaseLetters = /[A-Z]/g;
-        if(value.match(upperCaseLetters)) {
-            password_help.classList.remove("hidden");
-        } else {
-            password_help.classList.add("hidden");
-            return false;
-        }
-        // Validate numbers
-        var numbers = /[0-9]/g;
-        if(value.match(numbers)) {
-            password_help.classList.remove("hidden");
-        } else {
-            password_help.classList.add("hidden");
-            return false;
-        }
-        getForm(next_page);
+            
         } else{
                 getForm(next_page);
 
@@ -225,5 +215,124 @@ window.setAccountType = function accountType(type){
     localStorage.setItem("account_type", type);
     window.location.hash = "registration/form1.html";
 }
+function submitImages(){
+    return new Promise((resolve, reject)=>{
+        loading(false);
+        document.getElementById('cover').classList.remove("hidden");
+        let student_id_name = document.getElementById("");
+        let passport_photo_name = document.getElementById("");
+        let student_id_image = document.getElementById("passport_size_photo");
+        let passport_size_photo = document.getElementById("student_id_image");
+        // student_id_image = 
+    var uploadTask = firebase.storage().ref('profile_images/'+localStorage.getItem('email')+'/'+'student_id.png').put(files_id[0]);
+    document.getElementById('upload-helper-text').innerHTML = "0/2 <br> Uploading student Id...";
+    uploadTask.on('state_changed', function(snapshot){
+        let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100; 
+        console.log(progress);
+        updateProgressBar(progress);
+        if(progress == 100) {
+            console.log("Uploading 1 done");
+            document.getElementById('upload-helper-text').innerHTML = "1/2 <br> Uploading passport size photo";
+            let uploadTask2 = firebase.storage().ref('profile_images/'+localStorage.getItem('email')+'/'+'passport_size_photo.png').put(files_passport[0]);
+            uploadTask2.on('state_changed', function(snapshot){
+                let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100; 
+                updateProgressBar(progress);
+                if(progress === 100) {
+                    document.getElementById('upload-helper-text').innerHTML = "2/2 <br> Upload finished.";
+                    document.getElementById("cover").classList.add("hidden");
+                    loading(true);
+                    firebase.firestore().collection("campus-users").doc(localStorage.getItem('email')).set({
+                        email: localStorage.getItem('email'),
+                        student_id: localStorage.getItem('student-number'),
+                        first_name: localStorage.getItem('first_name'),
+                        last_name: localStorage.getItem('last_name'),
+                        phone_number: localStorage.getItem('phone_number'),
+                        account_type: localStorage.getItem('account_type'),
+                    }).then(() => {
+                        getPage("registration/success.html").then((data)=>{
+                            displayPage(data, page);
+                            loading(false);
+                        // ...
+                    }).catch((error)=>{
+                        console.log(error);
+                        loading(false);
+                    });
+                    
+                }).catch((error)=>{
+                    loading(false);
+                    var errorCode = error.code;
+                        var errorMessage = error.message;
+                        console.log(errorMessage);
+                        console.log(errorCode);
+                });
+                 
+                 
+                }
+            },
+            function(error){
+                console.log("error "+error);
+            }
+            );
+        }
+    },
+    function(error){
+        console.log("error "+error);
+        // reject(error);
+    }
+    );
+    });
+    
 
+}
+var files_id, files_passport, reader;
+
+window.saveLocalImg_student_id_image = function saveImageToLocal_student_id_image(){
+    let input = document.createElement("input");
+    input.type = "file";
+    input.setAttribute("required", "required");
+    input.id = "student_id_image";
+    let checkBox = document.getElementById("check-box-1");
+
+    input.onchange = e =>{
+        files_id = e.target.files;
+        reader = new FileReader();
+        console.log(files_id);
+        checkBox.value = "checked";
+        reader.onload = function(){
+            document.getElementById("preview_student_id").src = reader.result;
+        }
+        reader.readAsDataURL(files_id[0]);
+
+    }
+    input.click();
+}
+window.saveLocal_passport_size_photo_image = function saveLocal_passport_size_photo_image(){
+    let input = document.createElement("input");
+    input.type = "file";
+    input.setAttribute("required", "required");
+    input.id = "passport_size_photo";
+    let checkBox = document.getElementById("check-box-2");
+    input.onchange = e =>{
+        files_passport = e.target.files;
+        reader = new FileReader();
+        console.log(files_passport);
+        checkBox.value = "checked";
+        reader.onload = function(){
+            document.getElementById("preview_password_size_photo").src = reader.result;
+        }
+        reader.readAsDataURL(files_passport[0]);
+
+    }
+    input.click();
+}
+
+function updateProgressBar(value){
+    document.getElementById("progres-bar").style.width = value+"%";
+}
+window.switchToP8 = function switchToP8(){
+    let value = document.getElementById("check-box-1").value;
+    if(value === "checked"){
+        getForm("registration/form8.html");
+    }
+}
 });
